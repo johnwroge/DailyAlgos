2e18f2a3b746f920fde73da20712725d2ddc96b3
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-01-24 11:25:16 -0500

    added bst

--- challenges/bst_maxPathSum.js
+++ challenges/bst_maxPathSum.js
@@ -16,26 +16,36 @@ Given the root of a binary tree, return the maximum path sum of any non-empty pa
  * @param {TreeNode} root
  * @return {number}
  */
+//create an implicit parameter of an array containing -negative (initial maxPathSum)
  var maxPathSum = function(root, maxValue = [ -Infinity ]) {
+    //invoke pathSum helper function with root and current max value
     pathSum(root, maxValue);
-
+    //return final max value at index 0
     return maxValue[0];
 };
 
+//create helper function that takes a root and maxValue
 const pathSum = (root, maxValue) => {
+    //base case: if root is null then return 0
     const isBaseCase = root === null;
     if (isBaseCase) return 0;
-
+    //return the function invocation of dfs passing in root and dfs 
     return dfs(root, maxValue);
 }
 
+//create dfs helper function that takes a node and a max value
 const dfs = (node, maxValue) => {
+    //create a variable left and assign to the maximum of either 0, or the recursive call to previous helper 
+    //passing in node.left and the curretn max value
     const left = Math.max(0, pathSum(node.left, maxValue));
+     //create a variable right and assign to the maximum of either 0, or the recursive call to previous helper 
+    //passing in node.right and the curretn max value
     const right = Math.max(0, pathSum(node.right, maxValue));
+    //create a sum variable and assign to the left plus right plus current node value
     const sum = left + right + node.val;
-
+    //reassign the max value at the 0th index to be the maximum of either maxValue at 0 index or the sum
     maxValue[0] = Math.max(maxValue[0], sum);
-
+    //return the maximum of either the left or right ADDED to the current node.val 
     return Math.max(left, right) + node.val;
 }
 
