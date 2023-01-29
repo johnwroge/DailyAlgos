f2d872e9a037bb01ada6bd7b236ed55f8ea69d6e
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-01-28 21:50:39 -0500

    done

--- challenges/longest_repeating_replacement.js
+++ challenges/longest_repeating_replacement.js
@@ -33,3 +33,125 @@ s consists of only uppercase English letters.
  var characterReplacement = function(s, k) {
     
 };
+
+/*Maintain left and right pointer, max instances of a single char, and visit counts for each char.
+for each char in string
+increment visit count for this char
+if new visit count is higher than max, update max
+if length of current string without max char count is greater than k,
+then we know the new char made it such that there are more chars missing than can be replaced by k,
+so we will remove the first char
+and increment left pointer
+increment right pointer to look at next char.
+In the end, the answer is whatever the window size is. This is because we never shrink the window size.
+As we look at new chars, we increase the window size.
+Once we see we can no longer increase due to limitation of k, we slide the window forward.
+In these inbetween states, it's possible the window doesn't span a valid subset,
+but that doesn't matter because the window size at one point did span a valid set.
+Instead, we wait until there's a possibility of a better set, which is when there is a substring with more instances of some char.
+*/
+
+const characterReplacement = (s, k) => {
+  let left = 0;
+  let right = 0;
+  let maxCharCount = 0;
+  const visited = {};
+
+  while (right < s.length) {
+    const char = s[right];
+    visited[char] = visited[char] ? visited[char] + 1 : 1;
+
+    if (visited[char] > maxCharCount) maxCharCount = visited[char];
+
+    if (right - left + 1 - maxCharCount > k) {
+      visited[s[left]]--;
+      left++;
+    }
+
+    right++;
+  }
+
+  return right - left;
+};
+
+//v2
+
+var characterReplacement2 = function (s, k) {
+    let [left, right, longest, max] = new Array(4).fill(0);
+    const frequencyMap = new Array(26).fill(0);
+
+    while (right < s.length) {
+        const count = addRightFrequency(s, right, frequencyMap);
+
+        longest = Math.max(longest, count);
+
+        let window = right - left + 1;
+        const canSlide = k < window - longest;
+        if (canSlide) {
+            subtractLeftFrequency(s, left, frequencyMap);
+            left++;
+        }
+
+        window = right - left + 1;
+        max = Math.max(max, window);
+
+        right++;
+    }
+
+    return max;
+};
+
+const addRightFrequency = (s, right, map) => {
+    const char = s[right];
+    const index = getCode(char);
+
+    map[index]++;
+
+    return map[index];
+};
+
+const subtractLeftFrequency = (s, left, map) => {
+    const char = s[left];
+    const index = getCode(char);
+
+    map[index]--;
+
+    return map[index];
+};
+
+const getCode = (char) => char.charCodeAt(0) - 'A'.charCodeAt(0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- challenges/minimum_window_substring.js
+++ challenges/minimum_window_substring.js
@@ -44,6 +44,152 @@ s and t consist of uppercase and lowercase English letters.
  * @param {string} t
  * @return {string}
  */
- var minWindow = function(s, t) {
+ 
+//v1
+
+/*
+This is actually very similar to the Permutation in String problem. The difference is that we don't 
+match based on string length since we aren't finding an exact permutation/anagram. Instead, we want 
+the minimal substring which means that we move our window based on finding the smallest possible solution 
+that takes care of the counts of all the characters in t. However, instead of keeping track of checking the
+ exact count of each character each time we encounter a matching a character in s, we instead only need to
+  keep track of how many unique characters there are in t and when a unique character goes down to 0 
+  (or less, since having more characters than we need is okay too). Since the length only goes down each 
+  time a character reaches exactly 0, we will never reach the count of all unique characters until every unique
+   character has reached 0.
+
+The key insight is that once we find the first substring that satisfies the condition, we need to start moving
+ the left part of the window until we have the minimal substring that satisfies it since we don't necessarily 
+ need all the characters on the left. In the test case provided:
+
+s = "ADOBECODEBANC"
+t = "ABC"
+
+The minimal substring is just BANC, but we won't encounter it until we've covered all of s. At that point,
+ we need to trim our substring from the left until we get to the minimal substring which can be accomplished 
+ with a while loop that moves the left pointer while the substring condition is true.
+*/
+var minWindow = function(s, t) {
+    if (t.length > s.length) return '';
     
+    const neededChars = {};
+    
+    for (let char of t) {
+        neededChars[char] = (neededChars[char] || 0) + 1;
+    }
+    
+    let left = 0;
+    let right = 0;
+    let neededLength = Object.keys(neededChars).length;
+    let substring = '';
+    
+    while (right < s.length) {
+        const rightChar = s[right];
+        neededChars[rightChar]--;
+        if (neededChars[rightChar] === 0) neededLength--;
+        
+        while (neededLength === 0) {
+            if (!substring || substring.length > right - left + 1) {
+                substring = s.slice(left, right + 1);
+            }
+            
+            const leftChar = s[left];
+            // If the leftChar in charMap is at exactly 0 before being 
+            // incremented, we now need more leftChars so that its count
+            // in charMap goes down to exactly 0
+            if (neededChars[leftChar] === 0) {
+                neededLength++;
+            }
+            neededChars[leftChar]++;
+            left++;
+               
+        }
+        
+        right++;
+    }
+    
+    return substring;
 };
+
+
+//v2
+
+/**
+ * https://leetcode.com/problems/minimum-window-substring/
+ * Time O(N + M) | SpaceO(N + M)
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+ var minWindow = function (s, t) {
+    const isMissingArgs = !s.length || !t.length;
+    if (isMissingArgs) return '';
+
+    const frequencyMap = getFrequencyMap(t);
+    const { start, end } = getWindowPointers(s, t, frequencyMap);
+
+    return getSubString(s, start, end);
+};
+
+const getFrequencyMap = (str, frequencyMap = new Map()) => {
+    for (const char of str) {
+        frequencyMap.set(char, (frequencyMap.get(char) || 0) + 1);
+    }
+
+    return frequencyMap;
+};
+
+const getWindowPointers = (s, t, frequencyMap) => {
+    let [left, right, matched, start, end] = [0, 0, 0, 0, s.length + 1];
+
+    while (right < s.length) {
+        matched = addRightFrequency(s, right, frequencyMap, matched);
+
+        const canSlide = () => matched === t.length;
+        while (canSlide()) {
+            const window = right - left + 1;
+
+            const isSmaller = window < end;
+            if (isSmaller) {
+                [start, end] = [left, window];
+            }
+
+            matched = subtractLeftFrequency(s, left, frequencyMap, matched);
+            left++;
+        }
+
+        right++;
+    }
+
+    return { start, end };
+};
+
+const addRightFrequency = (s, right, frequencyMap, matched) => {
+    const char = s[right];
+
+    if (frequencyMap.has(char)) {
+        frequencyMap.set(char, frequencyMap.get(char) - 1);
+
+        const isInWindow = 0 <= frequencyMap.get(char);
+        if (isInWindow) matched++;
+    }
+
+    return matched;
+};
+
+const subtractLeftFrequency = (s, left, frequencyMap, matched) => {
+    const char = s[left];
+
+    if (frequencyMap.has(char)) {
+        const isOutOfWindow = frequencyMap.get(char) === 0;
+        if (isOutOfWindow) matched--;
+
+        frequencyMap.set(char, frequencyMap.get(char) + 1);
+    }
+
+    return matched;
+};
+
+const getSubString = (s, start, end) =>
+    end <= s.length ? s.slice(start, start + end) : '';
+
