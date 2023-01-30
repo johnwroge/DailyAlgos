ad204be5dbceea13d9f7280589803612d3b90ce0
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-01-29 20:15:52 -0500

    done

--- challenges/linked_remove_Nth.js
+++ challenges/linked_remove_Nth.js
@@ -37,5 +37,17 @@ The number of nodes in the list is sz.
  * @return {ListNode}
  */
  var removeNthFromEnd = function(head, n) {
-    
+    let arr = []
+    let res = new ListNode()
+    let copy = res;
+    while(head){
+        arr.push(head.val)
+        head = head.next;
+    }
+    for(let i =0; i<arr.length;i++){
+        if(arr.length-i == n) continue;
+        copy.next = new ListNode(arr[i])
+        copy = copy.next;
+    }
+    return res.next;
 };
