45c73ce95010e4d1cf3c83f1de71d5a5c6cb01df
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-02-05 22:38:00 -0500

    combination sum

---
+++ challenges/spiralMatrix.js
@@ -0,0 +1,29 @@
+
+/*
+Challenge 1
+Declare a function spiralMatrix that takes in a 2-dimensional array of 
+numbers representing an m * n matrix, and returns a 1-dimensional array 
+containing all the elements in spiral order starting from the top-left corner 
+(matrix[0][0]). For example:
+
+[[1, 2, 3]
+[4, 5, 6]
+[7, 8, 9]]
+
+Should return: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+*/
+
+
+function spiral(matrix) {
+    const arr = [];
+      
+    while (matrix.length) {
+      arr.push(
+        ...matrix.shift(),
+        ...matrix.map(a => a.pop()),
+        ...(matrix.pop() || []).reverse(),
+        ...matrix.map(a => a.shift()).reverse()
+      );
+    }
+    return arr;
+  }
