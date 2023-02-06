8f2145c43fee955674cf72b6d3d45ff0d5637dba
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-02-05 22:39:17 -0500

    added

--- challenges/combination_sum.js
+++ challenges/combination_sum.js
@@ -47,6 +47,24 @@ All elements of candidates are distinct.
  * @param {number} target
  * @return {number[][]}
  */
+
+//leetcode soln 1
  var combinationSum = function(candidates, target) {
-    
+    let res = []
+    let temp = []
+    let iterate = (index,sum)=>{
+        if(sum>target) return;
+        if(sum == target){
+            res.push([...temp])
+            return;
+        }
+        for(let i =index; i<candidates.length;i++){
+            if(candidates[i]>target)continue
+            temp.push(candidates[i])
+            iterate(i, sum+candidates[i])
+            temp.pop()
+        }
+    }
+    iterate(0,0)
+    return res;
 };
