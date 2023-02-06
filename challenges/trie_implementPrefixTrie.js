18f309717c6ce95e475df86cdf1714c16e9b7c6a
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-02-06 09:07:28 -0500

    added trie

--- challenges/trie_implementPrefixTrie.js
+++ challenges/trie_implementPrefixTrie.js
@@ -41,3 +41,39 @@ Constraints:
 word and prefix consist only of lowercase English letters.
 At most 3 * 104 calls in total will be made to insert, search, and startsWith.
 */
+
+var Trie = function() {
+    
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
