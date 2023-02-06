fa513c18605d218c307a9de6c5331af7890dbc11
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-02-06 09:22:10 -0500

    added

--- challenges/trie_implementPrefixTrie.js
+++ challenges/trie_implementPrefixTrie.js
@@ -77,3 +77,130 @@ Trie.prototype.startsWith = function(prefix) {
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
+
+
+// Neetcode
+ class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isWord = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    /* Time O(N) | Space O(N) */
+    insert(word, node = this.root) {
+        for (const char of word) {
+            const child = node.children[char] || new TrieNode();
+
+            node.children[char] = child;
+
+            node = child;
+        }
+
+        node.isWord = true;
+    }
+
+    /* Time O(N) | Space O(1) */
+    search(word, node = this.root) {
+        for (const char of word) {
+            const child = node.children[char] || null;
+
+            if (!child) return false;
+
+            node = child;
+        }
+
+        return node.isWord;
+    }
+
+    /* Time O(N) | Space O(1) */
+    startsWith(prefix, node = this.root) {
+        for (const char of prefix) {
+            const child = node.children[char] || null;
+
+            if (!child) return false;
+
+            node = child;
+        }
+
+        return true;
+    }
+}
+
+
+//sol2 
+
+var Trie = function() {
+    this.n = {};  
+    this.isWord = false;
+    return this;
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let curr = this;
+    for (let i = 0; i < word.length; i++) {
+        if (!curr.n[word[i]]) {
+            curr.n[word[i]] = new Trie(word[i]);
+        }
+        curr = curr.n[word[i]];
+    }
+    curr.isWord = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let curr = this;
+    for (let i = 0; i < word.length; i++) {
+        let char = word[i];
+        if (!curr.n[char]) {
+            return false;
+        }
+        curr = curr.n[char];
+    }
+    return curr.isWord;
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let curr = this;
+    for (let i = 0; i < prefix.length; i++) {
+        let char = prefix[i];
+        if (!curr.n[char]) {
+            return false;
+        }
+        curr = curr.n[char];
+    }
+    return true;
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
+
