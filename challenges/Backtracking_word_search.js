1ea93852397173d1bad3543b01fa90b993bbe47d
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-02-09 16:48:30 -0500

    added soln

--- challenges/Backtracking_word_search.js
+++ challenges/Backtracking_word_search.js
@@ -81,90 +81,91 @@ const dfs1 = (board, word, row, col, index) => {
                 dfs1(board, word, row, col - 1, index + 1)  
             ) return true;
             //either row + 1, row - 1, col + 1, col - 1 is true
+            board[row][col] = temp; 
     }
         //reassign the character in the board to be the store character
-    board[row][col] = temp; 
+    
 }
 
-
+console.log(exist1([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB"))
 //neetcode
 
-/**
- * https://leetcode.com/problems/word-search/
- * Time O(N * 3^L) | Space O(L)
- * @param {character[][]} board
- * @param {string} word
- * @return {boolean}
- */
- var exist = function(board, word) {
-    for(let row = 0; row < board.length; row++) {
-        for(let col = 0; col < board[0].length; col++){
-            if (dfs(board, row, col, word, 0)) return true;
-        }
-    }
+// /**
+//  * https://leetcode.com/problems/word-search/
+//  * Time O(N * 3^L) | Space O(L)
+//  * @param {character[][]} board
+//  * @param {string} word
+//  * @return {boolean}
+//  */
+//  var exist = function(board, word) {
+//     for(let row = 0; row < board.length; row++) {
+//         for(let col = 0; col < board[0].length; col++){
+//             if (dfs(board, row, col, word, 0)) return true;
+//         }
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-const dfs = (board, row, col, word, index) => {
-    if (index === word.length) return true;
-    if (isOutOfBound(board, row, col)) return false;
-    if (board[row][col] !== word[index]) return false
+// const dfs = (board, row, col, word, index) => {
+//     if (index === word.length) return true;
+//     if (isOutOfBound(board, row, col)) return false;
+//     if (board[row][col] !== word[index]) return false
         
-    board[row][col] = '*';
+//     board[row][col] = '*';
     
-    const hasWord = Object
-        .values(directions(row, col))
-        .filter(([r, c]) => dfs(board, r, c, word, index + 1))
-        .length
+//     const hasWord = Object
+//         .values(directions(row, col))
+//         .filter(([r, c]) => dfs(board, r, c, word, index + 1))
+//         .length
     
-    board[row][col] = word[index];
-    return hasWord;
-}
+//     board[row][col] = word[index];
+//     return hasWord;
+// }
 
-const isOutOfBound = (board, row, col) => {
-    const isRowOutOfBound = row < 0 || board.length - 1 < row
-    const isColOutOfBound = col < 0 || board[0].length - 1 < col 
-    return isRowOutOfBound || isColOutOfBound
-}
+// const isOutOfBound = (board, row, col) => {
+//     const isRowOutOfBound = row < 0 || board.length - 1 < row
+//     const isColOutOfBound = col < 0 || board[0].length - 1 < col 
+//     return isRowOutOfBound || isColOutOfBound
+// }
 
-const directions = (row, col) => ({
-    up: [row - 1, col],
-    down: [row + 1, col],
-    left: [row, col - 1,],
-    right: [row, col + 1]
-})
+// const directions = (row, col) => ({
+//     up: [row - 1, col],
+//     down: [row + 1, col],
+//     left: [row, col - 1,],
+//     right: [row, col + 1]
+// })
 
-//soln2 leetcode
+// //soln2 leetcode
 
-/**
- * @param {character[][]} board
- * @param {string} word
- * @return {boolean}
- */
- var exist = function(board, word) {
-    for (var i = 0; i < board.length; i++) {
-        for (var j = 0; j < board[0].length; j++) {
-            if (existHelper(board, word, i, j, 0)) return true;
-        }
-    }
+// /**
+//  * @param {character[][]} board
+//  * @param {string} word
+//  * @return {boolean}
+//  */
+//  var exist = function(board, word) {
+//     for (var i = 0; i < board.length; i++) {
+//         for (var j = 0; j < board[0].length; j++) {
+//             if (existHelper(board, word, i, j, 0)) return true;
+//         }
+//     }
 
-    return false;
-};
+//     return false;
+// };
 
-// if the word[k] existing in board, keep searching up, down, left, right 
-var existHelper = function(board, word, i, j, k) {
-    if (k === word.length) return true;
-    if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1) return false;
-    if (board[i][j] === word[k]) {
-        var tmp = board[i][j];
-        board[i][j] = '#';
-        if (existHelper(board, word, i + 1, j, k + 1) ||
-            existHelper(board, word, i - 1, j, k + 1) ||
-            existHelper(board, word, i, j + 1, k + 1) ||
-            existHelper(board, word, i, j - 1, k + 1)) {
-              return true;
-        }
-        board[i][j] = tmp;
-    }
-};
+// // if the word[k] existing in board, keep searching up, down, left, right 
+// var existHelper = function(board, word, i, j, k) {
+//     if (k === word.length) return true;
+//     if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1) return false;
+//     if (board[i][j] === word[k]) {
+//         var tmp = board[i][j];
+//         board[i][j] = '#';
+//         if (existHelper(board, word, i + 1, j, k + 1) ||
+//             existHelper(board, word, i - 1, j, k + 1) ||
+//             existHelper(board, word, i, j + 1, k + 1) ||
+//             existHelper(board, word, i, j - 1, k + 1)) {
+//               return true;
+//         }
+//         board[i][j] = tmp;
+//     }
+// };
---
+++ challenges/graph_cloneGraph.js
@@ -0,0 +1,95 @@
+/*
+133. Clone Graph
+Medium
+7.3K
+3K
+Companies
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+ 
+
+Example 1:
+
+
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+Example 2:
+
+
+Input: adjList = [[]]
+Output: [[]]
+Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 
+and it does not have any neighbors.
+Example 3:
+
+Input: adjList = []
+Output: []
+Explanation: This an empty graph, it does not have any nodes.
+ 
+
+Constraints:
+
+The number of nodes in the graph is in the range [0, 100].
+1 <= Node.val <= 100
+Node.val is unique for each node.
+There are no repeated edges and no self-loops in the graph.
+The Graph is connected and all nodes can be visited starting from the given node.
+*/
+
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+
+ var cloneGraph = function(node) {
+    if(node == null) return node;
+    const visited = new Map();
+    return cloneHelper(node,visited);
+};
+
+var cloneHelper = function(node,visited){
+    const newNode =new Node(node.val);
+    visited[node.val] = newNode;
+    for(const neighbor of node.neighbors){
+        if(!visited[neighbor.val]){
+            const newNeighbor = cloneHelper(neighbor,visited);
+            newNode.neighbors.push(newNeighbor);
+        }
+        else{
+            const newNeighbor = visited[neighbor.val];
+            newNode.neighbors.push(newNeighbor);
+        }
+    }
+    return newNode;
+}
