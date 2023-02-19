e0c416af3915516cb058328455324249d4a5f57a
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-02-18 23:19:57 -0500

    added

--- challenges/bst_constructfrom.js
+++ challenges/bst_constructfrom.js
@@ -43,6 +43,20 @@ inorder is guaranteed to be the inorder traversal of the tree.
  * @param {number[]} inorder
  * @return {TreeNode}
  */
- var buildTree = function(preorder, inorder) {
-    
+ var buildTree = function(P, I) {
+    let M = new Map()
+    for (let i = 0; i < I.length; i++)
+        M.set(I[i], i)
+    return splitTree(P, M, 0, 0, I.length-1)
 };
+
+var splitTree = function(P, M, pix, ileft, iright) {
+    let rval = P[pix],
+        root = new TreeNode(rval),
+        imid = M.get(rval)
+    if (imid > ileft)
+        root.left = splitTree(P, M, pix+1, ileft, imid-1)
+    if (imid < iright)
+        root.right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright)
+    return root
+}
