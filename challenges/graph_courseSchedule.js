d61c99a60e0f331b5ccba4abdcc2914e458605b0
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-03-01 22:25:30 -0500

    added

--- challenges/graph_courseSchedule.js
+++ challenges/graph_courseSchedule.js
@@ -46,6 +46,52 @@ All the pairs prerequisites[i] are unique.
     
 };
 
+var canFinish = function(numCourses, prerequisites) {
+    
+  const preMap = {};
+  const visited = {};
+  
+// build the adacency list
+  for(let i = 0; i< prerequisites.length; i++){
+      if(preMap[prerequisites[i][0]] === undefined){
+          preMap[prerequisites[i][0]] = [prerequisites[i][1]]
+      } else {
+          preMap[prerequisites[i][0]].push(prerequisites[i][1])
+      }
+  }
+  console.log(preMap)
+  const dfs = (node) => {
+      if(visited[node]){
+          return false;
+      }
+      if(preMap[node] !==undefined){
+          if (preMap[node].length === 0){
+              return true;
+          }
+      
+          visited[node] = true;
+          for(let val of preMap[node]){
+              if(!dfs(val)){
+                  return false
+              }
+          }
+          visited[node] = false;
+          
+          preMap[node] = [];
+      }
+      return true;
+
+  }
+  
+  for(let key in preMap){
+      if(!dfs(key)){
+          return false
+      }
+  }
+  return true
+
+};
+
 /**
  * https://leetcode.com/problems/course-schedule/
  * Time O((V)^2 + E) | Space O(V + E)
