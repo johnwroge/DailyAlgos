dcf4c92558b5d1248db9ae2a4f7be1399eaf9421
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-05-14 21:09:34 -0400

    added union find

--- Heap_Class/Heap.js
+++ Heap_Class/Heap.js
@@ -26,11 +26,54 @@ class MaxBinaryHeap {
         this.sinkDown()
     }
 
-    sinkDown(){
-        let counter = 0; 
-        
-
-    }
+    sinkDown (){
+        //declare an index variable initialized to 0 
+        let index = 0; 
+        //get length of this.values store in variable for readability
+        let length = this.values.length; 
+        //get the element in the 0th position 
+        let element = this.values[0];
+        //loop while true
+        while (true){
+            //calculate left (2*index + 1) and right (2*index + 2)
+            let left =  2 * index + 1; 
+            let right = 2 * index + 2; 
+            //intialize left and right child to undefined
+            let leftChild, rightChild; 
+            //create swap variable intitialzied to null
+            let swap = null; 
+            //check if left is valid (less than length)
+            if (left < length){
+                //assign left child to be the value at the left index in this.values
+                leftChild = this.values[left];
+                //if leftchild is greater than element
+                if (leftChild > element){
+                    //reassign swap to be left
+                    swap = left; 
+                }
+            }
+            //check if right is valid, less than length
+            if (right < length){
+                 //reassign rightchild to be the element at the right index
+                 rightChild = this.values[right];
+                 //check if swap is null and right child is greater than element 
+                 if (swap === null && rightChild > element ||
+                     //or if swap is not null and right child is greater than left child
+                     swap !== null && rightChild > leftChild
+                    ){//if true reassign swap to be right
+                        swap = right; 
+                    }  
+            }
+            
+            //check if swap is null (no swaps happened then you found right position)
+            if (swap === null) break; 
+            //assign the value at the index to be the value at the swap index
+            this.values[index] = this.values[swap];
+            //assign the value at the swap index to be the element
+            this.values[swap] = element; 
+            //reassign the index to be the swap index; 
+            index = swap; 
+        }
 
 }
 
--- Heaps/Heap.js
+++ Heaps/Heap.js
@@ -23,33 +23,52 @@ class MaxBinaryHeap {
                 }
         }
     sinkDown (){
+        //declare an index variable initialized to 0 
         let index = 0;
+        //get length of this.values store in variable for readability
         const length = this.values.length; 
+        //get the element in the 0th position 
         const element = this.values[0];
+        //loop while true
         while (true){
+            //calculate left (2*index + 1) and right (2*index + 2)
             let left = 2*index + 1;
             let right = 2*index + 2;
+            //intialize left and right child to undefined
             let leftChild, rightChild; 
+            //create swap variable intitialzied to null
             let swap = null;
+            //check if left is valid (less than length)
             if (left < length){
+                //assign left child to be the value at the left index in this.values
                 leftChild = this.values[left];
+                //if leftchild is greater than element
                 if (leftChild > element){
+                    //reassign swap to be left
                     swap = left;
                 }
             }
+            //check if right is valid, less than length
             if (right < length){
+                //reassign rightchild to be the element at the right index
                 rightChild = this.values[right];
+                //check if swap is null and right child is greater than element 
+                //or if swap is not null and right child is greater than left child
                 if (
                     (swap === null && rightChild > element) ||
                     (swap !== null && rightChild > leftChild)
                     )
-                {
+                {//if true reassign swap to be right
                     swap = right;
                 }
             }
+            //check if swap is null (no swaps happened then you found right position)
             if (swap === null) break;
+            //assign the value at the index to be the value at the swap index
             this.values[index] = this.values[swap];
+            //assign the value at the swap index to be the element
             this.values[swap] = element; 
+            //reassign the index to be the swap index; 
             index = swap;
         }
 
@@ -57,8 +76,11 @@ class MaxBinaryHeap {
         
     }
     extractMax (){
+        //store value of max = to first value
         const max = this.values[0];
+        //store the last value as end, and remove it
         const end = this.values.pop();
+
         if (this.values.length > 0){
             this.values[0] = end; 
             this.sinkDown(); 
