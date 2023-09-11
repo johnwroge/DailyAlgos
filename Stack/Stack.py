a3fe23e7e5647a9c76212a3a7b09f20d905b3c9b
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-09-11 08:54:26 -0400

    min stack

---
+++ Stack/Stack.py
@@ -0,0 +1,44 @@
+'''
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+
+'''
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# minStack =  MinStack()
+# print(minStack.push(-2))
+# print(minStack.push(0))
+# print(minStack.push(-3))
+# print(minStack.getMin())  #// return -3
+# print(minStack.pop())
+# print(minStack.top())   #// return 0
+# print(minStack.getMin()) #// return -2
