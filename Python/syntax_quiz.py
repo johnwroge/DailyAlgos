4232e62ba90ae482aa3de7ab8777f4352269f681
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-11-29 19:41:58 -0500

    added python test

---
+++ Python/syntax_quiz.py
@@ -0,0 +1,96 @@
+'''
+quiz for syntax
+'''
+
+# add an element to a specific index in an array
+
+# append item to list
+
+# remove item from list
+
+# remove item at index
+
+# find index of 1st occurence
+
+# find count of number of times item appears
+
+# sort a list
+
+# reverse a list
+
+# reverse a list 
+
+# create a new list of list1 and list 2
+
+# modify list 1 to contain items from list2
+
+# remove all elements from a list
+
+# reverse a string
+
+# change number to string
+
+# change string to lowercase
+
+# change string to uppercase
+
+# replace parts of string with new 
+
+# split a string by a separator
+
+# remove white space from string
+
+# concatenate two string use literal syntax
+
+# concatenate two strings use operator
+
+# create an adjacency list
+
+# create a deque
+
+# add a number to the left part of deque
+
+# pop an element from the left part of deque
+
+# create an object for counting items 
+
+# create a counter of an object
+
+# create new list of 2d array sorted by 1st element
+
+# create new list of 2d array sorted by 1st element in reverse
+
+# create a new list of tuples
+
+# create a new dictionary from list of tuples
+
+# get dictionaries keys
+
+# get dic values
+
+# get dict view object
+
+# remove item at particular key and return its value
+
+# remove all items from dictionary
+
+# get a particular value at a key
+
+# change dictionary key value with one from another 
+
+# remove a value
+
+# create a minimum heap from a list of numbers
+
+# create a maximum heap from list of numbers
+
+# create a set 
+
+# sort 2d array sorted by 1st element in place
+
+# create new array of length n filled with 0
+
+# convert string in binary to number
+
+
+
