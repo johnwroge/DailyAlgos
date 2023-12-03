702c02581dcc2141bb985baef573b5ed594dd836
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-02 20:26:17 -0500

    day11

---
+++ Project_Euler/11_largest_product_grid.py
@@ -0,0 +1,16 @@
+'''
+In the 20 x 20 grid below, four numbers along a diagonal line have been marked in red.
+The product of these 4 numbers is 26 63 78 14 = 1788696
+.
+What is the greatest product of four adjacent numbers in the same direction 
+(up, down, left, right, or diagonally) in the 20 x 20 grid?
+'''
+
+# [1,2,3,4]
+# [5,6,7,8]
+# [9,9,7,7]
+# [1,2,3,4]
+
+with open('/Users/johnwroge/DailyAlgos/Project_Euler/11_nums.txt') as f:
+    commands = f.readlines()
+
