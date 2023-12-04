49763cf0a17f08f1fee249c2979b9b44c9c4f96b
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-04 09:11:59 -0500

    unique paths

---
+++ Dynamic Programming/unique_paths.py
@@ -0,0 +1,11 @@
+
+def uniquePaths(m: int, n: int) -> int:
+    row = [1] * n
+    for i in range(m - 1):
+        new_row = [1] * n
+        for j in range(n - 2, -1, -1):
+            new_row[j] = new_row[j + 1] + row[j]
+        row = new_row
+    return row[0]
+
+print(uniquePaths(3,2))
