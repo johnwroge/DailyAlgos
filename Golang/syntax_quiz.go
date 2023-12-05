7d9f81d9c3e020db66f3c471f8c3f800d3d5da97
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-05 10:49:26 -0500

    adding syntax quiz

---
+++ Golang/syntax_quiz.go
@@ -0,0 +1,19 @@
+/*
+go get is used to add a module to the current program you’re working 
+on so you can import it. This is like npm install
+
+go install is used to install a go binary utility, so you can use 
+it as a normal tool on your system. This is more equivalent 
+to npm install -g. This isn’t used for importing a library, it’s 
+for installing a self-contained go program.
+*/
+package main
+
+import (
+	// "fmt"
+
+)
+
+func main(){
+
+}
