51e54f6a960a572ce5dc33dcddb652394c43ce6e
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-21 22:16:24 -0500

    syntax

--- Python/syntax_quiz.py
+++ Python/syntax_quiz.py
@@ -5,37 +5,52 @@ import collections
 import heapq
 import math
 thing = [1,2,3]
-list2 = [2,3,5,6]
+list2 = [2,3,5,1,0,2,22]
 
 # add an element to a specific index in an array
+# list2.insert(0,22)
+
 
 # append item to list
 
 # remove item from list
 
+
 # remove item at index
 
+
 # find index of 1st occurence
+# print(list2.index(2))
 
 # find count of number of times item appears
-
+print(list2.count(3))
 # sort a list
+list2.sort()
 
 # reverse a list
+list2.sort(key=lambda x: x, reverse=True)
 
 # reverse a list and return new list
+new_list = list(reversed(list2))
+# print(new_list)
 
 # create a new list of list1 and list 2
+list3 = list2 + list2
 
 # modify list 1 to contain items from list2
+list2.extend(list2)
 
 # remove all elements from a list
+# list2.clear()
 
 ''' Are strings mutable in python? '''
 
 # reverse a string using a list***
+s = 'abc'
+t = ''.join(list(reversed(s)))
 
 # reverse a string using slices***
+t2 = s[::-1]
 
 # change number to string
 
@@ -48,7 +63,7 @@ list2 = [2,3,5,6]
 # replace parts of string with new 
 
 # split a string by a separator
-
+s.split(' ')
 # split a string in a list of characters
 
 # remove white space from string
@@ -59,6 +74,9 @@ thing2 = 'def'
 # concatenate two strings use operator
 
 # create an adjacency list using a library with both a list and a set 
+
+adjList = collections.defaultdict(list)
+adjList2 = collections.defaultdict(set)
 
 '''what is the difference in time and space complexity for operations in a list and a set?'''
 
@@ -134,9 +152,18 @@ Sets
 
 # convert string in binary to number (remember what to include?)
 bin_string = '1010101010'
+num = int(bin_string, 2)
+# print(num)
+# find the intersection of 2 sets
+
+
 
 
 # take the product of a list of numbers
+arr = [1,2,3,4,5,6,7,8,9,10]
+# import math
+# p = math.prod(arr)
+
 
 # rotate a 2d grid by 90degrees
 # https://stackoverflow.com/questions/8421337/rotating-a-two-dimensional-array-in-python
@@ -144,5 +171,7 @@ bin_string = '1010101010'
 
 # what is the difference between a tuple, list and object? 
 
+# tuple is immutable so it requires less space
 
 # iterate over key val pairs in dictionary and return a list of these
+
